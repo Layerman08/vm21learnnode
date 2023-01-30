bec32fc1f25ed7dff3ba66f661f4b8069e96de54
Mairon Leier <u-47@tptlive.ee>
2023-01-30 14:55:33 +0200

    Add templating

--- index.js
+++ index.js
@@ -2,12 +2,38 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
+const nunjucks = require('nunjucks');
 
-app.get('/', (req, res) => {
-    //console.log(path.resolve(__dirname, 'index.html'));
-  res.send(path.resolve(__dirname, 'index.html'));
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
 });
 
+app.get('/', (req, res) => {
+  res.render('index.njk');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.njk');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact.njk');
+});
+
+app.get('/gallery', (req, res) => {
+  res.render('gallery.njk');
+});
+
+app.get('/values', (req, res) => {
+  res.render('values.njk');
+});
+
+app.get('/info', (req, res) => {
+  res.render('info.njk');
+});
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port https://localhost:${port}`);
 });
